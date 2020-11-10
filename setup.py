6d357a0e656efb37bffaa82976c62aeb50e58581
eggplants <w10776e8w@yahoo.co.jp>
2020-11-10 12:26:50 +0900

    Add files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dojinvoice_db",
+    version="0.0.1",
+    description="Make DB of Dojinvoice",
+    description_content_type="",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/eggplants/dojinvoice_db",
+    author="eggplants",
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    install_requires=open('requirements.txt').read().splitlines(),
+    entry_points={
+        "console_scripts": [
+            "dvdb=main:main"
+        ]
+    }
+)
