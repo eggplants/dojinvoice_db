e895a217b1439afe8036372e94c9b5e3e833cd63
eggplants <w10776e8w@yahoo.co.jp>
2022-05-29 10:13:38 +0900

    refactoring

--- out/selenium/webdriver/edge/service.pyi
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common import service as service
-from typing import Any
-
-class Service(service.Service):
-    service_args: Any
-    def __init__(self, executable_path, port: int = ..., verbose: bool = ..., log_path: Any | None = ...) -> None: ...
-    def command_line_args(self): ...
--- out/selenium/webdriver/ie/service.pyi
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common import service as service
-from typing import Any
-
-class Service(service.Service):
-    service_args: Any
-    def __init__(self, executable_path, port: int = ..., host: Any | None = ..., log_level: Any | None = ..., log_file: Any | None = ...) -> None: ...
-    def command_line_args(self): ...
