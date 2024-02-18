b98ac4d17afdf659a4717675ccbc6aea0476dc5a
eggplants <w10776e8w@yahoo.co.jp>
2024-02-18 19:48:03 +0900

    modernize

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,29 @@
-FROM python:3.11.1
+FROM python:3
+
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+ARG VERSION
+ENV VERSION ${VERSION:-master}
 
 # install google chrome
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN wget -q -O - "https://dl-ssl.google.com/linux/linux_signing_key.pub" | apt-key add -
 RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-RUN apt-get -y update
-RUN apt-get install -y google-chrome-stable
+# hadolint ignore=DL3008
+RUN apt-get -y update \
+    && apt-get install -y google-chrome-stable unzip --no-install-recommends \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
 # install chromedriver
-RUN apt-get install -yqq unzip
-RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN wget -q -O /tmp/chromedriver.zip "http://chromedriver.storage.googleapis.com/$(\
+    wget -q -O - https://chromedriver.storage.googleapis.com/LATEST_RELEASE \
+    )/chromedriver_linux64.zip"
 RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
 # set display port to avoid crash
 ENV DISPLAY=:99
 
 # pip
-RUN pip install -U pip && pip install dojinvoice-db
+RUN python -m pip --no-cache-dir install git+https://github.com/eggplants/dojinvoice_db@${VERSION}
 
-CMD ["/bin/bash"]
+CMD ["dvdb"]
--- dojinvoice_db/parser.py
+++ dojinvoice_db/parser.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 
-import os
 import urllib.parse
-from datetime import datetime as dt
+from datetime import datetime, timezone
+from pathlib import Path
 from random import uniform
 from time import sleep
 from typing import TypedDict, cast
 
-import pyderman
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
 from bs4.element import Tag
 from humanfriendly import parse_size
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
 
 
-class ParseUnknownSite(Exception):
+class UnknownSiteError(Exception):
     pass
 
 
@@ -79,12 +75,12 @@ class DmmDict(TypedDict):
 UA = {
     "User-Agent": "Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_5) "
     "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/67.0.3396.99 Safari/537.36"
+    "Chrome/67.0.3396.99 Safari/537.36",
 }
 
 
 class Parser:
-    def __init__(self, site: str, exclude_ids: list[str] = []) -> None:
+    def __init__(self, site: str, exclude_ids: list[str] | None = None) -> None:
         """Init."""
         self.site = site
         self.exclude_ids = exclude_ids
@@ -101,41 +97,29 @@ class Parser:
         options.add_argument("--start-maximized")
         options.add_argument("--user-agent={}".format(UA["User-Agent"]))
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("useAutomationExtension", False)  # noqa: FBT003
 
-        print("Preparing for headless chrome...", end="", flush=True)
+        print("Preparing for headless chrome...", end="", flush=True)  # noqa: T201
 
-        executable_path = pyderman.install(verbose=False)
-        self.driver = webdriver.Chrome(
-            executable_path=str(executable_path), options=options
-        )
+        self.driver = webdriver.Chrome(options=options)
         self.driver.set_page_load_timeout(60)
+        self.driver.get("https://www.dlsite.com")
+        self.driver.add_cookie({"name": "locale", "value": "ja-jp"})
+        self.driver.add_cookie({"name": "adultchecked", "value": "1"})
         self.driver.get("https://www.dlsite.com/maniax/work/=/product_id/RJ305341.html")
 
-        WebDriverWait(self.driver, 15).until(
-            expected_conditions.presence_of_element_located((By.CLASS_NAME, "btn_yes"))
-        )
-        btn = self.driver.find_element(By.CLASS_NAME, "btn-approval")
-        if btn:
-            btn.click()
-
-    def parse(self, path: str, page_idx: int = 0) -> list[DlsiteDict] | list[DmmDict]:
+    def parse(self, path: Path, page_idx: int = 0) -> list[DlsiteDict] | list[DmmDict]:
         """Extract required information from the page sources and scrape it."""
         self.page_idx = page_idx
         if self.site == "dlsite":
             return self.__parse_dlsite_pages(path)
-        # elif self.site == 'dmm':
-        #     return self.__parse_dmm_pages(path)
-        else:
-            raise ParseUnknownSite("Unknown Site: %s" % self.site)
+        raise UnknownSiteError("Unknown Site: %s" % self.site)
 
-    def __parse_dlsite_pages(self, path: str) -> list[DlsiteDict]:
+    def __parse_dlsite_pages(self, path: Path) -> list[DlsiteDict]:  # noqa: PLR0915
         res = []
-        bs = BS(open(path).read(), "lxml")
+        bs = BeautifulSoup(path.open().read(), "lxml")
         work_links = [
-            str(a.get("href"))
-            for a in bs.select("a[class=work_thumb_inner]")
-            if isinstance(a.get("href"), str)
+            str(a.get("href")) for a in bs.select("a[class=work_thumb_inner]") if isinstance(a.get("href"), str)
         ]
         thumb_links = [
             str(img.get("src") or img.get("data-src"))
@@ -143,18 +127,16 @@ class Parser:
             if isinstance(img.get("src"), str) or isinstance(img.get("data-src"), str)
         ]
         if len(work_links) != len(thumb_links):
-            raise ValueError(
-                "number of works != number of thumbnails: "
-                f"({len(work_links)} != {len(thumb_links)})"
-            )
+            msg = f"number of works != number of thumbnails: ({len(work_links)} != {len(thumb_links)})"
+            raise ValueError(msg)
         for idx, (work_link, thumb_link) in enumerate(zip(work_links, thumb_links)):
-            print(
-                "\33[2K\r{}: {}".format(100 * self.page_idx + idx + 1, work_link),
+            print(  # noqa: T201
+                f"\33[2K\r{100 * self.page_idx + idx + 1}: {work_link}",
                 end="",
             )
             o = urllib.parse.urlparse(work_link)
-            work_id = os.path.splitext(os.path.basename(o.path))[0]
-            if work_id in self.exclude_ids:
+            work_id = Path(o.path).stem
+            if self.exclude_ids and work_id in self.exclude_ids:
                 continue
             data = cast("DlsiteDict", {})
 
@@ -167,12 +149,12 @@ class Parser:
             else:
                 raise ConnectionError
 
-            bs = BS(self.driver.page_source, "lxml")
+            bs = BeautifulSoup(self.driver.page_source, "lxml")
             if bs is None:
                 continue
             title = bs.select_one("h1[id=work_name]")
             if title is None:
-                print("404 skipped:", work_link)
+                print("404 skipped:", work_link)  # noqa: T201
                 continue
 
             data["work_id"] = work_id
@@ -183,23 +165,13 @@ class Parser:
             data["thumbnail"] = f"https:{thumb_link}"
 
             circle_elm = bs.select_one("span[class=maker_name] > a")
-            data["circle"] = (
-                circle_elm.string
-                if circle_elm is not None and circle_elm.string
-                else ""
-            )
+            data["circle"] = circle_elm.string if circle_elm is not None and circle_elm.string else ""
 
             circle_link_elm = bs.select_one("span[class=maker_name] a")
-            data["circle_link"] = (
-                str(circle_link_elm.get("href", ""))
-                if circle_link_elm is not None
-                else ""
-            )
+            data["circle_link"] = str(circle_link_elm.get("href", "")) if circle_link_elm is not None else ""
 
             cien_elm = bs.select_one("div[class=link_cien] a")
-            data["cien_link"] = (
-                str(cien_elm.get("href", "")) if cien_elm is not None else None
-            )
+            data["cien_link"] = str(cien_elm.get("href", "")) if cien_elm is not None else None
 
             info_table = {
                 str(tr.th.string): tr.td
@@ -210,55 +182,35 @@ class Parser:
                 and tr.td is not None
             }
 
-            sale_date_elm = (
-                info_table["販売日"].select_one("a") if "販売日" in info_table else None
-            )
+            sale_date_elm = info_table["販売日"].select_one("a") if "販売日" in info_table else None
             sale_date = (
-                str(sale_date_elm.string)
-                if sale_date_elm is not None and sale_date_elm.string is not None
-                else ""
+                str(sale_date_elm.string) if sale_date_elm is not None and sale_date_elm.string is not None else ""
             )
 
             if "時" in sale_date:
                 data["sale_date"] = int(
-                    dt.strptime(sale_date, "%Y年%m月%d日 %H時").timestamp()
+                    datetime.strptime(sale_date, "%Y年%m月%d日 %H時").astimezone(timezone.utc).timestamp(),
                 )
             else:
-                data["sale_date"] = int(dt.strptime(sale_date, "%Y年%m月%d日").timestamp())
+                data["sale_date"] = int(
+                    datetime.strptime(sale_date, "%Y年%m月%d日").astimezone(timezone.utc).timestamp(),
+                )
 
-            category_elm = (
-                info_table["作品形式"].select_one("a") if "作品形式" in info_table else None
-            )
+            category_elm = info_table["作品形式"].select_one("a") if "作品形式" in info_table else None
             data["category"] = (
-                category_elm.string
-                if category_elm is not None and category_elm.string is not None
-                else ""
+                category_elm.string if category_elm is not None and category_elm.string is not None else ""
             )
 
-            series_elm = (
-                info_table["シリーズ名"].select_one("a") if "シリーズ名" in info_table else None
-            )
-            data["series"] = (
-                series_elm.string
-                if series_elm is not None and series_elm.string is not None
-                else ""
-            )
+            series_elm = info_table["シリーズ名"].select_one("a") if "シリーズ名" in info_table else None
+            data["series"] = series_elm.string if series_elm is not None and series_elm.string is not None else ""
 
             data["writers"] = (
-                [
-                    writer.string
-                    for writer in info_table["作者"].select("a")
-                    if writer.string is not None
-                ]
+                [writer.string for writer in info_table["作者"].select("a") if writer.string is not None]
                 if "作者" in info_table
                 else None
             )
             data["scenarios"] = (
-                [
-                    scenario.string
-                    for scenario in info_table["シナリオ"].select("a")
-                    if scenario.string is not None
-                ]
+                [scenario.string for scenario in info_table["シナリオ"].select("a") if scenario.string is not None]
                 if "シナリオ" in info_table
                 else None
             )
@@ -271,39 +223,19 @@ class Parser:
                 if "イラスト" in info_table
                 else None
             )
-            data["voices"] = (
-                [_.string for _ in info_table["声優"].select("a")]
-                if "声優" in info_table
-                else None
-            )
-            data["musicians"] = (
-                [_.string for _ in info_table["音楽"].select("a")]
-                if "音楽" in info_table
-                else None
-            )
+            data["voices"] = [_.string for _ in info_table["声優"].select("a")] if "声優" in info_table else None
+            data["musicians"] = [_.string for _ in info_table["音楽"].select("a")] if "音楽" in info_table else None
             data["age_zone"] = ",".join(
-                [
-                    age_zone.string
-                    for age_zone in info_table["年齢指定"].select("span")
-                    if age_zone.string is not None
-                ]
+                [age_zone.string for age_zone in info_table["年齢指定"].select("span") if age_zone.string is not None],
             )
             data["file_format"] = info_table["ファイル形式"].get_text()
             data["genres"] = (
-                [
-                    genre.string
-                    for genre in info_table["ジャンル"].select("a")
-                    if genre.string is not None
-                ]
+                [genre.string for genre in info_table["ジャンル"].select("a") if genre.string is not None]
                 if "ジャンル" in info_table
                 else None
             )
             file_size_elm = info_table["ファイル容量"].select_one("div[class=main_genre]")
-            file_size = (
-                file_size_elm.string
-                if file_size_elm is not None and file_size_elm.string is not None
-                else ""
-            )
+            file_size = file_size_elm.string if file_size_elm is not None and file_size_elm.string is not None else ""
             data["file_size"] = parse_size(file_size.replace("計", "").replace("総", ""))
 
             trial_elm = bs.select_one("div[class*=trial_download] > ul > li")
@@ -312,8 +244,7 @@ class Parser:
                 trial_link_elm = trial_elm.select_one("a[class=btn_trial]")
                 data["trial_link"] = (
                     f"https:{trial_link_elm.get('href')}"
-                    if trial_link_elm is not None
-                    and isinstance(trial_link_elm.get("href"), str)
+                    if trial_link_elm is not None and isinstance(trial_link_elm.get("href"), str)
                     else None
                 )
                 trial_size_elm = trial_elm.select_one("span")
@@ -324,18 +255,12 @@ class Parser:
                 )
 
             description_elm = bs.select_one("div[itemprop=description]")
-            data["description"] = (
-                description_elm.get_text() if description_elm is not None else ""
-            )
+            data["description"] = description_elm.get_text() if description_elm is not None else ""
 
             data["monopoly"] = bs.select_one("span[title=DLsite専売]") is not None
 
             rate_elm = bs.select_one("span[class='point average_count']")
-            data["rating"] = (
-                float(rate_elm.string)
-                if rate_elm is not None and rate_elm.string is not None
-                else None
-            )
+            data["rating"] = float(rate_elm.string) if rate_elm is not None and rate_elm.string is not None else None
 
             sales_elm = bs.select_one("dd[class=point]")
             data["sales"] = (
@@ -351,9 +276,9 @@ class Parser:
                 else None
             )
 
-            price_elm = bs.select_one("div[class=work_buy_content]")
+            price_elm = bs.select_one("div[class=price]")
             data["price"] = (
-                int(price_elm.string.replace(",", "").replace("円", ""))
+                int(price_elm.text.replace(",", "").replace("円", ""))
                 if price_elm is not None and price_elm.string is not None
                 else 0
             )
@@ -365,11 +290,10 @@ class Parser:
                 else None
             )
 
-            sleep(uniform(0.1, 1.0))
+            sleep(uniform(0.1, 1.0))  # noqa: S311
+
+            # breakpoint()  # noqa: ERA001
+
             res.append(data)
 
         return res
-
-    # def __parse_dmm_pages(path: str) -> List[DmmDict]:
-    #     print(path, end="\r")
-    #     bs = BS(open(path, 'r'), 'lxml')
